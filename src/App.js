3963ea9eaaf5bf42f5b7072a55c7f99ec7dbce9d
Phanindra <phanindragadireddy2@gmail.com>
2025-03-17 15:40:09 +0530

    styles added

--- src/App.js
+++ src/App.js
@@ -2,52 +2,67 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [movies, setMovies] = useState('')
-  const [data, setData] = useState([])
+  const [movies, setMovies] = useState('');
+  const [data, setData] = useState([]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    fetch(`http://www.omdbapi.com/?s=${movies}&apikey=263d22d8`).then(response => response.json()).then(data => setData(data.Search))
-  }
+    fetch(`http://www.omdbapi.com/?s=${movies}&apikey=263d22d8`)
+      .then(response => response.json())
+      .then(data => setData(data.Search || []));
+  };
 
-  const download = url => {
-    fetch(url).then(response => {
-        response.arrayBuffer().then(function(buffer) {
-          const url = window.URL.createObjectURL(new Blob([buffer]));
-          const link = document.createElement("a");
-          link.href = url;
-          link.setAttribute("download", "image.png");
-          document.body.appendChild(link);
-          link.click();
-        });
+  const download = (url) => {
+    fetch(url)
+      .then(response => response.arrayBuffer())
+      .then(function (buffer) {
+        const url = window.URL.createObjectURL(new Blob([buffer]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "poster.png");
+        document.body.appendChild(link);
+        link.click();
       })
-      .catch(err => {
-        console.log(err);
-      });
+      .catch(err => console.log(err));
   };
 
   return (
     <center>
-      <h1>Movie Poster App</h1>
-      <br></br>
-      <form>
-        <input type="text" id="movieInput" placeholder="Enter movie title" value={movies} onChange={(e) => { setMovies(e.target.value) }} />
-        <input type='submit' onClick={submitHandler} />
+      <h1>🎬 Movie Poster Hub 🎥</h1>
+
+      {/* Search Bar */}
+      <form className="search-container" onSubmit={submitHandler}>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="🔎 Find your favorite movie posters..."
+          value={movies}
+          onChange={(e) => setMovies(e.target.value)}
+        />
+        <button type="submit" className="search-btn">Search</button>
       </form>
-      <div className='row'>
-      {data.map(movie => 
-      <div className='col-md-4'>
-      <div class="card" style={{ width: "18rem" }}>
-        <img src={movie.Poster} class="card-img-top" alt={movie.Title} />
-          <div class="card-body">
-            <h3 className='card-title'>{movie.Title}</h3>
-            <a className='btn btn-primary' onClick={() =>download(movie.Poster)}> Download Poster</a>
-          </div>
-      </div>
-      </div>
+
+      {/* Clean Placeholder Section */}
+      {data.length === 0 ? (
+        <div className="placeholder">
+          <p>✨ Discover <span>epic movie posters</span> in high resolution!</p>
+          <p>🎬 Type a movie name and start exploring.</p>
+        </div>
+      ) : (
+        <div className="row">
+          {data.map((movie) => (
+            <div className="col-md-4" key={movie.imdbID}>
+              <div className="card">
+                <img src={movie.Poster} alt={movie.Title} />
+                <div className="card-body">
+                  <h3 className="card-title">{movie.Title}</h3>
+                  <a className="btn-primary" onClick={() => download(movie.Poster)}>⬇ Download Poster</a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       )}
-      </div>
-      
     </center>
   );
 }
